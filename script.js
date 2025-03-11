b3c87216012e701fe30dece7de99567e3d680703
Lakshan-16 <4al21ec042@gmail.com>
2025-03-11 12:01:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log(window);
+window.console.log("hello "); // window object is always present in a web page and the web page knows it 
+//  when we create a html.file it will create object called document inside the js file and that document is sub object of the window object 
